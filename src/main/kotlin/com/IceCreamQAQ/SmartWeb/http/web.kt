6a7ca55cadfc43fe54dc131142e9cee45229a8d5
IceCream <www@withdata.net>
2022-08-13 00:17:02 +0800

    添加 Method Enum，并使 Request 携带 Method Enum

--- src/main/kotlin/com/IceCreamQAQ/SmartWeb/http/web.kt
+++ src/main/kotlin/com/IceCreamQAQ/SmartWeb/http/web.kt
@@ -2,5 +2,25 @@ package com.IceCreamQAQ.SmartWeb.http
 
 import com.IceCreamQAQ.YuWeb.H
 
-interface Request : H.Request
+interface Request : H.Request {
+    val methodEnum: Method?
+}
+
 interface Response : H.Response
+
+enum class Method {
+    GET, POST, PUT, UPDATE;
+
+    companion object {
+        @JvmStatic
+        fun valueOrNullOf(str: String) = when (str) {
+            "get" -> GET
+            "post" -> POST
+            "put" -> PUT
+            "UPDATE" -> UPDATE
+            else -> null
+        }
+
+        fun String.toHttpMethodOrNull() = valueOrNullOf(this)
+    }
+}
